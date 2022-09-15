f1d2d5877fdcf3955b7ed0bfdb42b7468bfb54b9
codersalman <coder88salman@gmail.com>
2022-09-15 08:50:13 +0530

    dynamic rating function

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const mainContainer = document.querySelector(".container-main")
-const thanksContainer = document.querySelector(".thankyou")
-const submitButton = document.getElementById("submit")
-const rateAgain = document.getElementById("rate-again")
-const rates1 = document.querySelector("btn1")
-const rates2 = document.querySelector("btn2")
-const rates3 = document.querySelector("btn3")
-const rates4 = document.querySelector("btn4")
-const rates5 = document.querySelector("btn5")
 
-submitButton.addEventListener("click", () =>{
+function rate(a){
 
-    thanksContainer.classList.remove("hidden1")
-    mainContainer.style.display = "none"
-})
+    var rateScore = a;
+    
+    if (!rateScore) {
+      console.log('Sorry');
+    }else{
+    
+      console.log(rateScore);
+      document.getElementById('score').innerHTML = `You selected ${rateScore} out of 5`;
 
-rateAgain.addEventListener("click", () =>{
-    thanksContainer.classList.add("hidden1")
-    mainContainer.style.display = "block"
-})
-
-
-
+    }
+    
+    const mainContainer = document.querySelector(".container-main")
+    const thanksContainer = document.querySelector(".thankyou")
+    const submitButton = document.getElementById("submit")
+    const rateAgain = document.getElementById("rate-again")
+    
+    
+    submitButton.addEventListener("click", () =>{
+    
+        thanksContainer.classList.remove("hidden1")
+        mainContainer.style.display = "none"
+    })
+    
+    rateAgain.addEventListener("click", () =>{
+        thanksContainer.classList.add("hidden1")
+        mainContainer.style.display = "block"
+    })
+    
+    
+    }
+    
+    
